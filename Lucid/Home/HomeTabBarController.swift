0cb1116cf89816d176a2a698ce17df00590b3b3c
Andrew Breckenridge <asbreckenridge@me.com>
2015-02-28 03:33:15 -0600

    subclass the tabBar for style

---
+++ Lucid/Home/HomeTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeTabBarController.swift
+//  Lucid
+//
+//  Created by Andrew Breckenridge on 2/28/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import UIKit
+
+class HomeTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        tabBar.tintColor = UIColor.whiteColor()
+    }
+}
